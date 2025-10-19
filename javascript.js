63b68c210fcbb5122f5c096dfba6725017d80272
Sisu <stefansisu785@gmail.com>
2025-10-20 00:46:58 +0300

    Created the computer's choice function.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello tuturor");
+function getComputerChoice() {
+    if(Math.random() < 0.33) {
+        return "Paper";
+    } else if(Math.random() < 0.66)
+                return "Rock";
+                else return "Scissors";
+}
+
+console.log(getComputerChoice());
+
