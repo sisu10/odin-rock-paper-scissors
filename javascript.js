d19b1599a929f427ecb3017a6f09cfd0357634ec
Sisu <stefansisu785@gmail.com>
2025-10-20 01:18:16 +0300

    Created playRound function.
    It's just the beginning. The game works only once, then resets.

--- javascript.js
+++ javascript.js
@@ -6,5 +6,57 @@ function getComputerChoice() {
                 else return "Scissors";
 }
 
-console.log(getComputerChoice());
+let humanChoice = prompt("You wanna play as (Rock/Paper/Scissors):")
 
+console.log(humanChoice);
+
+let humanScore, computerScore;
+humanScore = 0;
+computerScore = 0;
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice == "Rock") {
+        if(computerChoice == "Scissors") {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+        }
+        else if(computerChoice == "Paper") {
+            console.log("You lose! Paper beats Rock.");
+            computerScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+
+    if(humanChoice == "Scissors") {
+        if(computerChoice == "Paper") {
+            console.log("You win! Scissors beat Paper.");
+            humanScore++;
+        }
+        else if(computerChoice == "Rock") {
+            console.log("You lose! Rock beats Scissors.");
+            computerScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+
+    if(humanChoice == "Paper") {
+        if(computerChoice == "Rock") {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+        }
+        else if(computerChoice == "Scissors") {
+            console.log("You lose! Scissors beats Paper.");
+            computerScore++;
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+}
+
+playRound(humanChoice, computerChoice);
+console.log(humanScore)
+console.log(computerScore)
